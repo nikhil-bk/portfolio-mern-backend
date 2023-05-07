4e6398d881abe52fd198e2ce00b9580aa35d498c
Nikhil bk <nikhilbk9148@gmail.com>
2023-05-07 12:05:54 +0530

    cors issue

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ PORT =process.env.PORT || 5000;
 // static assets
 if(process.env.NODE_ENV==='production'){
     app.use(express.static('client/build'));
-    app.get('*', (req, res)=>res.sendFile(path.resolve(__dirname, 'temp')))
+    app.get('*', (req, res)=>res.sendFile(path.resolve(__dirname, 'tmp')))
 }
 
 
