ad1ed403decda88dabf6043e72462ddf4e0e71cc
Nikhil bk <nikhilbk9148@gmail.com>
2023-05-07 12:22:16 +0530

    cors issue

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use(cors({
 }));
 app.use(fileUpload({
     useTempFiles:true,
-    tempFileDir: path.join(process.cwd(),'tmp')
+    tempFileDir: path.join(process.cwd(),'./tmp')
     
 }))
 
@@ -80,7 +80,7 @@ PORT =process.env.PORT || 5000;
 // static assets
 if(process.env.NODE_ENV==='production'){
     app.use(express.static('client/build'));
-    app.get('*', (req, res)=>res.sendFile(path.resolve(__dirname, 'tmp')))
+    app.get('*', (req, res)=>res.sendFile(path.resolve(process.cwd(),'tmp')))
 }
 
 
